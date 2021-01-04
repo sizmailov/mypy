faf00630d406f61024aa4d085454d035bc81b4ca
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2021-01-05 00:10:06 +0300

    [stubgenc] Show exact types for class attributes

--- mypy/stubgenc.py
+++ mypy/stubgenc.py
@@ -316,7 +316,7 @@ def generate_c_type_stub(module: ModuleType,
         if is_skipped_attribute(attr):
             continue
         if attr not in done:
-            variables.append('%s: Any = ...' % attr)
+            variables.append('%s: %s = ...' % (attr, strip_or_import(type(value).__name__, module, imports)))
     all_bases = obj.mro()
     if all_bases[-1] is object:
         # TODO: Is this always object?
